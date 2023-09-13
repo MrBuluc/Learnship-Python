528aa1f1406379d04b2ffbb49e536d8f9da10c95
U180709061 <hakkicanbuluc@gmail.com>
2023-09-13 21:46:31 +0300

    13.9

---
+++ main.py
@@ -0,0 +1,80 @@
+import datetime
+
+
+""" print("Hello Yusuf")
+
+if 5 > 2:
+    print("5 greater than 2")
+#        print("Wrong Dounle Tab")
+
+if 5 > 2:
+        print("Double Tab")
+        
+x = 5
+y = "Yunus Yıldırım"
+
+print(x)
+print(y)
+
+x = "Learnship"
+y = 10
+
+print("------------") # ayraç
+print(x)
+print(y)
+
+# int x = 5;
+# x = "djoıasjod" error
+
+x = """
+#dasdöişsaödiş
+#lsaşdöşlasd
+#dölaşsömdşlsa
+"""
+ """
+ 
+# x = int(str(5))
+# y = float("3.14")
+# print(type(y))
+# if type(y) is float:
+#     print('Float')
+
+# yusufYildirimAge = 5 # Camel Case
+
+# YusufYildirimAge = 10 # Pascal Case
+
+# yusuf_yildirim_age = 15 # Snake Case # Python
+
+# x, y, z = 5, "10", True # x = 5, y = "10", z = True
+# print(x)
+# print(y)
+# a = int(z)
+# print(a) # 1
+
+# a = b = c = "Learn"
+
+# a = [1, 2 ,3, 4]
+# x, y, z, q = a
+# print(x)
+# print(y)
+# print(z)
+
+# a = (1, 2, 3)
+# #a[0] = 2
+# print(a)
+
+# yunus = {"age": 10, "boy": 180, "sex": "Erkek"}
+# for a in yunus.keys(): # ["age", "boy", "sex"] 
+#     print(a + ": " + str(yunus[a]))
+    
+# a = {1, 2, 1, 258, 58, 2, 1}
+# print(a)
+# True
+# False
+# None
+# a = 1.e-2
+# print(a)
+
+# a = 3 + 5j # Complex No
+# print(a)
+
